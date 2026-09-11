9a8942be923d442436e438f615fc5ad89a0e376f
rudiaaloo <rudrakshi.wangoo@syk.fi>
2026-09-11 10:00:18 +0300

    commit10

---
+++ 5. functions/examples/example-1.py
@@ -0,0 +1,4 @@
+def abs_value(x):
+    if x < 0:
+        return -x
+    return x
---
+++ course1/6. using_classes_and_modules/examples/example_3.py
@@ -0,0 +1,4 @@
+s = "thingamabob"
+for c in s: #typical to use c becuz it reverses characters or something
+    if c > 'd':
+        print(c, end = ' ') #this is like a comparison with different charcters 
