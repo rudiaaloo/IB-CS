37ba38a0077d2b27e1f124c0b97fda4cef2cdd83
rudiaaloo <rudrakshi.wangoo@syk.fi>
2026-09-11 08:39:07 +0300

    coomit7

--- lists/example2_lists.py
+++ lists/example2_lists.py
@@ -3,7 +3,11 @@ lst = []
 lst.append(nonnegative_integer) 
 print(lst)
 while (v := int(input("pls enter a nongetaive integer: "))) >= 0: #u could do without the variable but wlarus saves a whole line 
-   #if v < 0:
+  i = 0 
+  while i < len(lst) and lst[i] < v: #boundary test if u are still within the list bounds
+    i += 1
+  lst.insert (i, v) #if v < 0:
        #there is a while so no need to put the if 
-    lst.append(v) 
-    print(lst)
+ print(lst)
+
+#didnt need extra rules for going to begining, or null list 1,2,4 HW 
--- lists/list_searching.py
+++ lists/list_searching.py
@@ -6,3 +6,6 @@ print(lst)
 val = 8 #this would give us how many times 8 is in the list, which is 1 time
 print(lst.count(val)) 
 print(lst.index(val)) #this would give us the index of the first occurrence of 8 in the list, which is 7
+#this is from list class which we havent exactly covered yet 
+
+i = 0 #indexbased traversal 
